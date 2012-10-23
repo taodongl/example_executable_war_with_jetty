b3e8b5fb57670a894cfe7f840ca62d0f5a90de46
Taodong Lu <taodongl@ubuntu.(none)>
2012-10-23 23:38:41 +0800

    add home parameter

--- jettyExam/src/main/java/Runner.java
+++ jettyExam/src/main/java/Runner.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.net.URL;
 import java.security.ProtectionDomain;
 import org.eclipse.jetty.server.Server;
@@ -7,12 +8,16 @@ public class Runner {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		int port = Integer.parseInt(System.getProperty("port", "8080"));
+		final int port = Integer.parseInt(System.getProperty("port", "8080"));
+		final String home = System.getProperty("home","");
 		Server server = new Server(port);
 		ProtectionDomain domain = Runner.class.getProtectionDomain();
 		URL location = domain.getCodeSource().getLocation();
 		WebAppContext webapp = new WebAppContext();
 		webapp.setContextPath("/");
+		if (home.length() != 0) {
+			webapp.setTempDirectory(new File(home));
+		}
 		webapp.setWar(location.toExternalForm());
 		server.setHandler(webapp);
 		server.start();
